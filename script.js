8960c3b865580c154a87ad38da5ea8aff8471611
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-05-24 22:35:32 +0200

    add script

---
+++ script.js
@@ -0,0 +1,16 @@
+// Participant 1
+const login1 =document.getElementById("login1")
+const message1 = document.querySelector("#message1")
+const btnsend1 = document.querySelector("#btn-send1")
+
+// Particpant 2
+const login2 = document.getElementById("login2")
+const message2 = document.querySelector("#message2")
+const btnsend2 = document.querySelector("#btn-send2")
+
+// Zone d'affichage
+const dispalyText = document.getElementById("display-text")
+
+// Boutons d'action
+const reset = document.getElementById("reset")
+const stop = document.getElementById("stop")
