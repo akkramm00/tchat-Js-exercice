5e851cc7ae4757d33c782b399f644dcb86129d40
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-05-24 23:00:03 +0200

    add second script

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ const dispalyText = document.getElementById("display-text")
 // Boutons d'action
 const reset = document.getElementById("reset")
 const stop = document.getElementById("stop")
+
+btnsend1.addEventListener('click', () => setTimeout(function(e){
+    displayMessage1(e)
+}, 2000))
+btnsend2.addEventListener('click', () => setTimeout(function(e){
+    displayMessage2(e)
+}, 2000))
+
+function displayMessage1(e) {
+    dispalyText.innerHTML += '<p class="message">'+login1.value+' dit '+message1.value+'</p>'
+}
+function displayMessage2(e) {
+    dispalyText.innerHTML += '<p class="message">'+login2.value+' dit '+message2.value+'</p>'
+}
+
+btnreset.addEventListener('click', reset)
+
+function reset() {
+    dispalyText.innerHTML = ''
+}
+
+stop.addEventListener('click', stopMessage)
+function stopMessage(e) {
+    e.preventDefault()
+    btnsend2.setAttribute('disabled', 'disabled')
+    btnsend2.removeEventListener('click', displayMessage2)
+}
